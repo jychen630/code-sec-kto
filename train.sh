6dad0d51f914ed84d3fb19d0d4a479bcd2850d3d
Junyao Chen <junyao.chen@nyu.edu>
2024-11-09 23:38:28 -0500

    Export env vars to train scripts

--- train.sh
+++ train.sh
@@ -1,2 +1,21 @@
-export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512
-python train.py loss=kto model=codellama7b datasets=[bigvul] exp_name=first_test ++cache_dir=/local/nlp/junyao/huggingface ++model.name_or_path=codellama/CodeLlama-7b-hf
+#!/bin/bash
+export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:128
+export CUDA_VISIBLE_DEVICES=4,7
+export HYDRA_FULL_ERROR=1
+
+# debug for FSDP (Fully Sharded Data Parallel)
+export NCCL_DEBUG=INFO
+export NCCL_IB_DISABLE=1
+export NCCL_P2P_DISABLE=1
+
+# huggingface cache dir
+export TRANSFORMERS_CACHE="/local/nlp/junyao/huggingface"
+export HF_HOME="/local/nlp/junyao/huggingface"
+export HF_DATASETS_CACHE="/local/nlp/junyao/huggingface"
+
+python train.py \
+    loss=kto \
+    model=starcoder \
+    datasets=[bigvul] \
+    exp_name=first_test \
+    ++cache_dir=/local/nlp/junyao/huggingface \
