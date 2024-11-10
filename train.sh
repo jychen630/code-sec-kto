836a07ceda1283b653412db52a1b933f9bf6360e
Junyao Chen <junyao.chen@nyu.edu>
2024-11-10 05:03:29 -0500

    Update training shell script

--- train.sh
+++ train.sh
@@ -8,14 +8,26 @@ export NCCL_DEBUG=INFO
 export NCCL_IB_DISABLE=1
 export NCCL_P2P_DISABLE=1
 
+# FSDP optimizations
+export FSDP_SHARDING_STRATEGY=1  # FULL_SHARD
+export FSDP_STATE_DICT_TYPE=SHARDED
+export FSDP_OFFLOAD_PARAMS=auto
+export FSDP_AUTO_WRAP_POLICY=size_based
+export FSDP_BACKWARD_PREFETCH=BACKWARD_POST
+export FSDP_CPU_OFFLOAD=true
+
 # huggingface cache dir
 export TRANSFORMERS_CACHE="/local/nlp/junyao/huggingface"
 export HF_HOME="/local/nlp/junyao/huggingface"
 export HF_DATASETS_CACHE="/local/nlp/junyao/huggingface"
 
+timestamp=$(date +"%Y%m%d_%H%M%S")
+model="codellama7b"
+comment=""
+
 python train.py \
     loss=kto \
-    model=starcoder \
+    model=${model} \
     datasets=[bigvul] \
-    exp_name=first_test \
+    ++exp_name="${timestamp}_${model}_${comment}" \
     ++cache_dir=/local/nlp/junyao/huggingface \
